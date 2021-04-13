34c9d801360acc1c5336729cfa19e290f6dcdfe2
SurabhiBits <2020mt93260@wilp.bits-pilani.ac.in>
2021-04-13 15:19:05 +0530

    added Header and Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [];
+//{ [path: 'dashboard', component: 'DahboardComponent'}}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
